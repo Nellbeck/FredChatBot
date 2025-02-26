ef406c40fce28e3a11ccaf582593fa2b9dab9db4
fredrik nellbeck <Foggel@live.se>
2025-02-26 07:39:54 +0100

    Updated connection string

--- chatbot-app/src/Chatbot.js
+++ chatbot-app/src/Chatbot.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
 function Chatbot() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
